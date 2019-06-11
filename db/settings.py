9578125f275dd8d848e22163c6663b3922bb6ec7
Allison Carrigan <allison.carrigan@yale.edu>
2019-06-10 22:19:19 -0400

    Added django-heroku, updated dependencies

--- db/settings.py
+++ db/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,5 @@ LOGIN_REDIRECT_URL = '/'
 
 # From Templates
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+django_heroku.settings(locals())
