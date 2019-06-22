8f2dd1e0426a2bc45b4a0d5bf1734015b1c84aa4
Allison Carrigan <allison.carrigan@yale.edu>
2019-06-22 14:33:53 -0400

    added empty_values

--- db_app/forms.py
+++ db_app/forms.py
@@ -9,7 +9,7 @@ class ArtistForm(forms.ModelForm):
 	def __init__(self, *args, **kwargs):
 		super(ArtistForm, self).__init__(*args, **kwargs)
 		self.fields['region_id'].label = 'Region'
-		self.fields['region_id'].empty_label = 'Choose a region...'
+		self.fields['region_id'].widget.attrs['title'] = 'Choose a region...'
 		self.fields['region_id'].widget.attrs['class'] = 'selectpicker'
 		self.fields['region_id'].widget.attrs['data-live-search'] = 'true'
 
@@ -42,7 +42,7 @@ class EditForm(forms.ModelForm):
 		super(EditForm, self).__init__(*args, **kwargs)
 		self.fields['status'].widget.attrs['class'] = 'selectpicker'
 		self.fields['region_id'].label = 'Region'
-		self.fields['region_id'].empty_label = 'Choose a region...'
+		self.fields['region_id'].widget.attrs['title'] = 'Choose a region...'
 		self.fields['region_id'].widget.attrs['class'] = 'selectpicker'
 		self.fields['region_id'].widget.attrs['data-live-search'] = 'true'
 
@@ -57,7 +57,7 @@ class RegionForm(forms.ModelForm):
 		fields = ('region', )
 
 class SearchForm(forms.Form):
-	name = forms.CharField(empty_value="Search by Name", required=False)
+	name = forms.CharField(required=False)
 	regions = forms.ModelMultipleChoiceField(widget=forms.SelectMultiple, queryset=region.objects.all(), required=False)
 	tags = forms.ModelMultipleChoiceField(widget=forms.SelectMultiple, queryset=tag.objects.all(), required=False)
 
@@ -67,14 +67,14 @@ class SearchForm(forms.Form):
 		for key in self.fields:
 			self.fields[key].label = ''
 
-		self.fields['name'].widget.attrs['placeholder'] = 'Enter an artist or band name...'
+		self.fields['name'].widget.attrs['placeholder'] = 'Search by name...'
 		
 		self.fields['regions'].widget.attrs['title'] = 'Select a region...'
 		self.fields['regions'].widget.attrs['class'] = 'selectpicker'
 		self.fields['regions'].widget.attrs['data-live-search'] = 'true'
 		self.fields['regions'].widget.attrs['data-actions-box'] = 'true'
 
-		self.fields['tags'].empty_label = 'Select tags...'
+		self.fields['tags'].widget.attrs['title'] = 'Select tag(s)...'
 		self.fields['tags'].widget.attrs['data-live-search'] = 'true'
 		self.fields['tags'].widget.attrs['class'] = 'selectpicker'
 		self.fields['tags'].widget.attrs['data-actions-box'] = 'true'
