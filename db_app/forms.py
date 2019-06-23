e2ba7f4a031c3838ccd19d6fde963f25c1234cdb
Allison Carrigan <allison.carrigan@yale.edu>
2019-06-22 22:20:55 -0400

    remove repetitive code

--- db_app/forms.py
+++ db_app/forms.py
@@ -67,18 +67,22 @@ class SearchForm(forms.Form):
 
 		for key in self.fields:
 			self.fields[key].label = ''
+			if key is not 'name':
+				self.fields[key].widget.attrs['data-live-search'] = 'true'
+				self.fields[key].widget.attrs['data-actions-box'] = 'true'
+				self.fields[key].widget.attrs['class'] = 'selectpicker'
 
 		self.fields['name'].widget.attrs['placeholder'] = 'Search by name...'
 		
 		self.fields['regions'].widget.attrs['title'] = 'Select a region...'
-		self.fields['regions'].widget.attrs['class'] = 'selectpicker'
-		self.fields['regions'].widget.attrs['data-live-search'] = 'true'
-		self.fields['regions'].widget.attrs['data-actions-box'] = 'true'
+		# self.fields['regions'].widget.attrs['class'] = 'selectpicker'
+		# self.fields['regions'].widget.attrs['data-live-search'] = 'true'
+		# self.fields['regions'].widget.attrs['data-actions-box'] = 'true'
 
 		self.fields['tags'].widget.attrs['title'] = 'Select tag(s)...'
-		self.fields['tags'].widget.attrs['data-live-search'] = 'true'
-		self.fields['tags'].widget.attrs['class'] = 'selectpicker'
-		self.fields['tags'].widget.attrs['data-actions-box'] = 'true'
+		# self.fields['tags'].widget.attrs['data-live-search'] = 'true'
+		# self.fields['tags'].widget.attrs['class'] = 'selectpicker'
+		# self.fields['tags'].widget.attrs['data-actions-box'] = 'true'
 
 
 
